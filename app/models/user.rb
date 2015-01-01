effe9b3ae6e8fa68044820f9ff9285d2cd046776
Stephan Bönnemann <stephan@boennemann.me>
2015-01-01 17:42:23 +0100

    feat(user): add username and allow to login with username/email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :contribs
   has_many :interests
+
+  validates_presence_of :username
+  validates :username,
+    :uniqueness => {
+      :case_sensitive => false
+    },
+    :format => {
+      :with => /\A[\w-]+\z/
+    }
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231164417) do
+ActiveRecord::Schema.define(version: 20150101160810) do
 
   create_table "contribs", force: true do |t|
     t.string   "title"
@@ -43,9 +43,11 @@ ActiveRecord::Schema.define(version: 20141231164417) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
