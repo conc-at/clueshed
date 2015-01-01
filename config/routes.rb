c3e9a44e33bfb4e354a92612a02e161e08b7859c
Stephan Bönnemann <stephan@boennemann.me>
2015-01-01 20:37:16 +0100

    feat(users): add "users/:username" route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
 
-  resources :interests
-
-  resources :contribs
+  devise_for :users
+  resources :users, only: [:show], param: :username
+  resources :interests, :contribs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
