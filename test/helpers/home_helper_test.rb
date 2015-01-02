606cd4158e7f8aa714333b8b0872b7a71f780fd5
David Thomas Grübl <me@davidgruebl.com>
2015-01-02 13:11:26 +0100

    test(home): partip helper

--- test/helpers/home_helper_test.rb
+++ test/helpers/home_helper_test.rb
@@ -6,4 +6,9 @@ class HomeHelperTest < ActionView::TestCase
     assert_equal placeholder('interest'), 'What are you intersted in?'
     assert_equal placeholder(''), nil
   end
+
+  test 'should return instance' do
+    assert_instance_of Contrib, partip('contrib')
+    assert_instance_of Interest, partip('interest')
+  end
 end
