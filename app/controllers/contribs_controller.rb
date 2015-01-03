74bb9fce12f8ffc48ef7348ac43e114da6a81964
David Thomas Grübl <me@davidgruebl.com>
2015-01-03 17:42:53 +0100

    feat(partip): allow to reply to interest with contrib

--- app/controllers/contribs_controller.rb
+++ app/controllers/contribs_controller.rb
@@ -1,2 +1,10 @@
 class ContribsController < PartipsController
+  # GET /contribs/new
+  def new
+    super
+
+    if Interest.exists?(:id => params[:in_reply_to])
+      @contrib.interest = Interest.find(params[:in_reply_to])
+    end
+  end
 end
