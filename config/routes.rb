6c59bc466a8733eebef2a7d50ecfca144abda755
Stephan Bönnemann <stephan@boennemann.me>
2015-01-03 22:59:16 +0100

    feat(votes): #create and #destroy and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   resources :users, only: [:show], param: :username
+  resources :votes, only: [:create, :destroy]
   resources :interests, :contribs
 
   # The priority is based upon order of creation: first created -> highest priority.
