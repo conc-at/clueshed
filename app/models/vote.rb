786ea06f58f6725f38bc415359012bf4fee35211
Stephan Bönnemann <stephan@boennemann.me>
2015-01-04 01:37:36 +0100

    feat(user): render list of upvoted partips

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -12,6 +12,14 @@ class Vote < ActiveRecord::Base
     self.uid = "#{self.user_id}_#{partip_type}_#{partip_id}"
   end
 
+  def partip
+    if self.interest
+      self.interest
+    elsif self.contrib
+      self.contrib
+    end
+  end
+
   private
     def has_one_partip
       unless (!self.interest.nil? and self.contrib.nil?) or (!self.contrib.nil? and self.interest.nil?)
@@ -22,14 +30,6 @@ class Vote < ActiveRecord::Base
     def is_not_own_partip
       if self.user_id == partip.user.id
         errors.add(:base, "You must not vote on your own Contrib or Interest.")
-      end
-    end
-
-    def partip
-      if self.interest
-        self.interest
-      elsif self.contrib
-        self.contrib
       end
     end
 
