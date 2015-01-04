c93ef17aefbd8b81698553c36294f0480e9f017b
Stephan Bönnemann <stephan@boennemann.me>
2015-01-04 15:09:50 +0100

    test(application): test flash_class helper

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'should return correct bootstrap flash class' do
+    assert_equal flash_class('notice'), 'alert-info'
+    assert_equal flash_class('success'), 'alert-success'
+    assert_equal flash_class('error'), 'alert-danger'
+    assert_equal flash_class('alert'), 'alert-warning'
+    assert_equal flash_class(''), nil
+  end
+end
