e9fdc49113169a7a87c26f3f618491e2236069bb
Stephan Bönnemann <stephan@boennemann.me>
2015-01-26 16:59:01 +0100

    refactor(contribs): controller

--- app/controllers/contribs_controller.rb
+++ app/controllers/contribs_controller.rb
@@ -2,10 +2,7 @@ class ContribsController < PartipsController
   # GET /contribs/new
   def new
     super
-
-    # Interest.exists?(params[:in_reply_to])
-    # aber: per bspw. find_by oder find_by_id könnte man sich auch den zusätzlichen DB-Call sparen
-    if Interest.exists?(:id => params[:in_reply_to])
+    if params[:in_reply_to]
       @contrib.interest = Interest.find(params[:in_reply_to])
     end
   end
