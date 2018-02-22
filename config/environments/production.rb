5ddd5b80bb73e82d40ba139d9797efc08717d74d
Hannes Moser <hannes@impossiblearts.com>
2018-02-22 18:40:47 +0100

    fix: use env variable for APP_DOMAIN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.app_domain = 'clueshed2018.herokuapp.com'
+  config.app_domain = ENV["APP_DOMAIN"]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'clueshed2018.herokuapp.com',
+  domain:               ENV["APP_DOMAIN"],
   user_name:            ENV["GMAIL_USERNAME"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
